c8ad5bb3744e4881cae2a123278f93445c52c0ff
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-08-04 22:50:19 -0300

    Testing implementations

---
+++ Sample/Teste.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample
+{
+    public class Teste
+    {
+        public static TValor Valor => new TValor();
+    }
+
+    public class TValor
+    {
+        public TOutroValor OutroValor => new TOutroValor();
+        public string Texto = "Oi";
+    }
+
+    public class TOutroValor
+    {
+        public TEsteValor EsteValor => new TEsteValor();
+    }
+
+    public class TEsteValor
+    {
+        public string Texto = "Teste.Valor.OutroValor.EsteValor";
+    }
+}
